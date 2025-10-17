65ca75017072c7842164960d834ac2acb806c6b6
vv824470-code <vv824470@gmail.com>
2025-10-17 15:49:16 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(e) {
+  console.log('Якутский Календарь установлен');
+});
+
+self.addEventListener('fetch', function(e) {
+  // Пустой, но нужен для PWA
+});
